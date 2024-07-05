895dda7d6519c039a9077fe47e5bd386b27848e0
bhoomika123098 <161302821+bhoomika123098@users.noreply.github.com>
2024-07-05 05:39:44 -0400

    Update one.java

--- one.java
+++ one.java
@@ -3,7 +3,7 @@ public class one
 	public static void main(String [] args)
 	{
 		System.out.println("hello tumkur");
-	        System.out.println("gubbi,mysore,banglore");
+	        System.out.println("gubbi,mysore,banglore,mmmmm");
 	}
 }
 
